62a5ce0da084b7eafaf2035cc367c86e7bbede6f
F3licia <romeofelicia@gmail.com>
2021-04-15 12:10:09 +0200

    corretto

--- Pari e dispari/myScriptPariDispari.js
+++ Pari e dispari/myScriptPariDispari.js
@@ -27,39 +27,34 @@ var computerNum = parseInt( Math.floor(Math.random() * 5) + 1);
     console.log("il computer ha prodotto " + computerNum)
 
 //creo una funzione per addizionare i valori
-function addizione(userNum, computerNum) {
-    var risultato = userNum + computerNum;
+function addizione(num1, num2) {
+    var risultato = num1 + num2;
     return risultato;
 }
 var somma = addizione(userNum, computerNum)
     console.log("la somma è " + somma)
 
-//funzione per calcolare se la somma è pari
-function pari(somma) {
-	if (somma %2 == 0)
-		return true;
-	else
-		return false;
-}
 
-    if(pari = true){
+	if (somma %2 == 0){
+		pari = true;
         console.log("è pari")
-    } else if (pari = false){
+	}else {
+		pari = false;
         console.log("è dispari")
     }
 
+var userWin
 
 //confronto il risultato con l'input dell'utente e ne stabilisco le condizioni per vincere
-function userWin() {
-	if (((userChoice === "pari") && (pari == true)) || ((userChoice === "dispari") && (pari == false))){
-     return true
-    } else { return false }
-}
-var messaggio = userWin()
 
-    if(userWin = true) {
-    console.log("l'utente ha vinto")
-    } else if (userWin = false) {
-    console.log("l'utente ha perso")
-    }   
+	if (((userChoice === "pari") && (pari === true)) || ((userChoice === "dispari") && (pari === false))){
+     userwin = true
+     console.log("l'utente ha vinto")
+    } else {
+        userwin = false 
+        console.log("l'utente ha perso")
+    }
+
+
+
 
